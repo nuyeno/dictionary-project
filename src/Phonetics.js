4ffc67586e35add8eba948976ae452da1955716b
nuyeno <101528194+nuyeno@users.noreply.github.com>
2022-07-02 22:45:08 -1000

    integrated photo api

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  console.log(props.phonetics);
   return (
     <div className="phonetics">
       <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
         Listen
-        <i class="fa-regular fa-circle-play"></i>
+        <i className="fa-regular fa-circle-play"></i>
       </a>
       <span>{props.phonetics.text}</span>
     </div>
