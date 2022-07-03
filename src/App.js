cec749360d7d457b70d5f48d5315ca342f3e6d28
nuyeno <101528194+nuyeno@users.noreply.github.com>
2022-07-02 17:22:09 -1000

    cont. styling, added default keyword search

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunrise" />
         </main>
         <footer>
           Coded by Naomi Uyeno
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,36 +3,54 @@ import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [definition, setDefinition] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setDefinition(response.data[0]);
   }
-  function search(event) {
-    event.preventDefault();
 
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  return (
-    <div className="wordSearch">
-      <section>
-        <form onSubmit={search}>
-          <input
-            type="search"
-            placeholder="Search a word"
-            autoFocus="on"
-            autoComplete="off"
-            onChange={handleKeywordChange}
-          />
-        </form>
-      </section>
-      <Results results={definition} />
-    </div>
-  );
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="wordSearch">
+        <section>
+          <p>What word would you like to look up?</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Search a word"
+              autoFocus="on"
+              autoComplete="off"
+              onChange={handleKeywordChange}
+            />
+          </form>
+        </section>
+        <Results results={definition} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
